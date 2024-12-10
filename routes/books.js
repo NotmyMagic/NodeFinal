169ef23a2220b63c7c54b3cee0149e0a5c7de016
Cale Sumner <calecsumner@gmail.com>
2024-12-10 12:59:40 -0700

    functional CRUD

--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,10 @@ const booksController = require("../controllers/index");
 
 router.get("/", booksController.getAllBooks);
 
+router.patch("/update/:id", booksController.updateBook);
+
+router.delete("/delete/:id", booksController.deleteBook);
+
 router.get("/:id", booksController.getOneBook);
 
 router.post("/", booksController.createBook);
